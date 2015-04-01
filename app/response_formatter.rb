0a6f50a7cc0233a554ff68b1bc8400c38c9e033b
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-01 18:12:23 -0400

    add code to make the ResponseFormatter#status_ok? method work

--- app/response_formatter.rb
+++ app/response_formatter.rb
@@ -1,2 +1,12 @@
 class ResponseFormatter
+  attr_reader :google_api_data
+
+  def initialize(google_api_data)
+    @google_api_data = google_api_data
+  end
+
+  def status_ok?
+    return google_api_data["status"] == "OK"
+  end
+
 end
