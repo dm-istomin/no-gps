23c1b7099fcbfb918b66174f1aaf636c645351a8
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-01 18:16:55 -0400

    move ResponseFormatter tests into the correct file

--- spec/response_formatter_spec.rb
+++ spec/response_formatter_spec.rb
@@ -1,12 +1,19 @@
 require_relative 'spec_helper'
 
-describe "Parser" do
+describe "ResponseFormatter" do
   before(:each) do
     path = File.dirname(__FILE__) + "/dummy_json/transit_response.json"
     file = File.open(path, "rb")
     contents = file.read
     file.close
-    
-    @google_api_response = JSON.parse(contents)
+
+    google_api_response = JSON.parse(contents)
+    @formatter = ResponseFormatter.new(google_api_response)
+  end
+
+  describe "#status_ok?" do
+    it "should return true when the Google Maps API returns the correct data" do
+      expect(@formatter.status_ok?).to be true
+    end
   end
 end
