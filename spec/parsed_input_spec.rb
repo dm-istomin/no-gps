cb444239a274b4bdc62299651a4e80814d1307c1
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 17:34:03 -0400

    tests for ParsedInput class

--- spec/parsed_input_spec.rb
+++ spec/parsed_input_spec.rb
@@ -1,7 +1,29 @@
 require_relative 'spec_helper'
 
 describe "ParsedInput" do
-  describe "#origin"
-  describe "#destination"
+  before(:each) do
+    @well_formatted_input = ParsedInput.new("from penn station, new york to grand central, new york")
+    @well_formatted_shorthand_input = ParsedInput.new("penn station, new york to grand central, new york")
+  end
+  describe "#origin" do
+    it "should get the correct input from a text" do
+      expect(@well_formatted_input.origin).to eq "penn station, new york"
+    end
+
+    it "should get the correct input from a text that omits the 'from' keyword" do
+      expect(@well_formatted_shorthand_input.origin).to eq "penn station, new york"
+    end
+  end
+
+  describe "#destination" do
+    it "should get the correct input from a text" do
+      expect(@well_formatted_input.destination).to eq "grand central, new york"
+    end
+
+    it "should get the correct input from a text that omits the 'from' keyword" do
+      expect(@well_formatted_shorthand_input.destination).to eq "grand central, new york"
+    end
+  end
+  
   describe "#mode"
 end
