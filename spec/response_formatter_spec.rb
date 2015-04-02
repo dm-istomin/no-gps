a6fc1c9522965ee9cb80770e6679f4a563ccc620
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 13:53:40 -0400

    add test for walking directions

--- spec/response_formatter_spec.rb
+++ spec/response_formatter_spec.rb
@@ -25,9 +25,13 @@ describe "ResponseFormatter" do
     it "should return transit directions in a simple, readable format" do
       formatter = ResponseFormatter.new(@google_api_response[:transit])
       expect(formatter.directions)
-      .to eq "[21 mins, 4.2 mi]: Head southeast on Wall St toward William St. Turn left onto William St. At 6:51pm, take 2 from Wall St Subway towards Wakefield - 241 St; exit at Times Sq - 42 St. Head northeast on 7th Ave toward W 42nd St. Turn right onto W 47th St. Walk until you reach Theater District, New York, NY, USA"
+      .to eq "[21 mins, 4.2 mi]: Head southeast on Wall St toward William St. Turn left onto William St. At 6:51pm, take 2 from Wall St Subway towards Wakefield - 241 St; exit at Times Sq - 42 St. Head northeast on 7th Ave toward W 42nd St. Turn right onto W 47th St. Walk until you reach Theater District, New York, NY, USA."
     end
 
-    it "should return walking directions in a simple, readable format"
+    it "should return walking directions in a simple, readable format" do
+      formatter = ResponseFormatter.new(@google_api_response[:walking])
+      expect(formatter.directions)
+      .to eq "[43 mins, 3.4 km]: Head southwest toward Place du Carrousel. Exit the roundabout onto Place du Carrousel. Turn right onto Quai François Mitterrand. Slight left onto Pont Royal. Turn right onto Quai Anatole France. Slight right. Slight right. Slight right onto Voie Expresse Rive Gauche. Slight left to stay on Voie Expresse Rive Gauche. Continue onto Quai Branly. Take the crosswalk. Walk until you reach Champ de Mars, Eiffel Tower, 5 Avenue Anatole France, 75007 Paris, France."
+    end
   end
 end
