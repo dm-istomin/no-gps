ecaaeb35377923118335dec57edebf1e0c399263
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 13:03:30 -0400

    modify ResponseFormatter tests to handle various types of JSON dummy data

--- spec/response_formatter_spec.rb
+++ spec/response_formatter_spec.rb
@@ -1,26 +1,33 @@
 require_relative 'spec_helper'
 
 describe "ResponseFormatter" do
-  before(:each) do
-    path = File.dirname(__FILE__) + "/dummy_json/transit_response.json"
-    file = File.open(path, "rb")
-    contents = file.read
-    file.close
+  before(:all) do
+    @google_api_response = {}
+    file_location = File.dirname(__FILE__) + "/dummy_json"
 
-    google_api_response = JSON.parse(contents)
-    @formatter = ResponseFormatter.new(google_api_response)
+    Dir.foreach(file_location) do |item|
+      next if item == "." || item == ".."
+      file = File.open("#{file_location}/#{item}", "rb")
+      contents = file.read
+      @google_api_response[item.match(/\w+/).to_s.to_sym] = JSON.parse(contents)
+      file.close
+    end
   end
 
   describe "#status_ok?" do
     it "should return true when the Google Maps API returns the correct data" do
-      expect(@formatter.status_ok?).to be true
+      formatter = ResponseFormatter.new(@google_api_response[:transit])
+      expect(formatter.status_ok?).to be true
     end
   end
 
   describe "#directions" do
-    it "should return directions in an easy-to-understand format" do
-      expect(@formatter.directions)
+    it "should return transit directions in a simple, readable format" do
+      formatter = ResponseFormatter.new(@google_api_response[:transit])
+      expect(formatter.directions)
       .to eq "[21 mins, 4.2 mi]: Head southeast on Wall St toward William St. Turn left onto William St. At 6:51pm, take 2 from Wall St Subway towards Wakefield - 241 St; exit at Times Sq - 42 St. Head northeast on 7th Ave toward W 42nd St. Turn right onto W 47th St. Walk until you reach Theater District, New York, NY, USA"
     end
+
+    it "should return walking directions in a simple, readable format"
   end
 end
