c1d9a3e6ccb2fcfc808332d5126341ca21ac63ae
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-01 20:18:31 -0400

    add some basic methods to generate formatted output in ResponseFormatter

--- app/response_formatter.rb
+++ app/response_formatter.rb
@@ -9,4 +9,33 @@ class ResponseFormatter
     google_api_data["status"] == "OK" || false
   end
 
+  def directions
+    "[#{trip_time}, #{trip_distance}]: #{trip_steps}"
+  end
+
+  def trip_time
+    google_api_data["routes"][0]["legs"][0]["duration"]["text"]
+  end
+
+  def trip_distance
+    google_api_data["routes"][0]["legs"][0]["distance"]["text"]
+  end
+
+  def trip_steps
+    format_trip_steps(google_api_data["routes"][0]["legs"][0]["steps"])
+  end
+
+  private
+
+  def clean_html(html_string)
+    html_string.gsub(/<[^>]*>/, "")
+  end
+
+  def format_trip_steps(steps, directions = [])
+    steps.each do |current_step|
+      directions << current_step["html_instructions"]
+      format_trip_steps(current_step["steps"], directions) if current_step["steps"]
+    end
+    clean_html(directions.join(". "))
+  end
 end
