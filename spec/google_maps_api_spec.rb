d16a070395b65b2c71db18fde2453aaeea2716d4
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 15:58:32 -0400

    update indentation in GoogleMapsAPI spec

--- spec/google_maps_api_spec.rb
+++ spec/google_maps_api_spec.rb
@@ -2,9 +2,9 @@ describe "GoogleMapsAPI" do
   describe "#get_directions" do
     before(:all) do
       GoogleMapsAPI.get_directions(
-      from: "smithsonian air and space museum, washington d.c",
-      to: "white house rose garden, washington d.c",
-      mode: "transit"
+        from: "smithsonian air and space museum, washington d.c",
+        to: "white house rose garden, washington d.c",
+        mode: "transit"
       )
     end
 
