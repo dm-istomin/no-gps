2a9d1dfe62744c9533918a11adcaf6da25999d20
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 16:46:50 -0400

    add code to make the GoogleMapsAPI#get_directions method function properly

--- app/google_maps_api.rb
+++ app/google_maps_api.rb
@@ -1,4 +1,10 @@
 module GoogleMapsAPI
-  def self.get_directions(args)
+  def self.get_directions(from:, to:, mode: "transit")
+    request = RestClient.get "https://maps.googleapis.com/maps/api/directions/json",
+              params: {mode: mode,
+                       origin: from,
+                       destination: to,
+                       key: ENV['GOOGLE_API']}
+    JSON.parse(request)
   end
 end
