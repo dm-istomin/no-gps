b1d39ff413360a193cd6a3d1ecb0e9bc370c82a5
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-03 00:27:57 -0400

    create route in the main.rb controller to return a response to queries for direction

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,3 +1,22 @@
 get "/" do
   erb :test
 end
+
+get "/directions" do
+  error_msg = "Not enough addresses; cannot give directions until two are provided!"
+  begin
+    query = ParsedInput.new(params[:query])
+  rescue ArgumentError
+    return error_msg
+  end
+
+  if query.valid?
+    directions = GoogleMapsAPI.get_directions(from: query.origin,
+                                              to: query.destination,
+                                              mode: query.mode)
+    result = ResponseFormatter.new(directions)
+    return result.directions
+  else
+    return error_msg
+  end
+end
