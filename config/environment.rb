0e358cacdc2992f5d001ee0aa937936cc074f905
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-03 14:50:00 -0400

    add eventual location of public folder to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,8 @@ APP_NAME =  APP_ROOT.basename.to_s
 
 # Sinatra config settings
 set :root, File.join(APP_ROOT, 'app')
-set :views, Proc.new { File.join(root, "views") }
+set :views, Proc.new { File.join(root, 'views') }
+set :public_folder, 'public'
 
 # Require all files inside the lib/ and app/controllers directories
 Dir[File.join(APP_ROOT, 'lib', '*.rb')].each { |file| require file }
