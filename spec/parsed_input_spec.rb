c1db94dc1ddf1d08db92f81c4b8f8b27be888b34
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2015-04-02 20:16:12 -0400

    change parsed_input_spec to have simpler tests, will need to fine-tune reg exs to handle more complex input in future

--- spec/parsed_input_spec.rb
+++ spec/parsed_input_spec.rb
@@ -2,28 +2,24 @@ require_relative 'spec_helper'
 
 describe "ParsedInput" do
   before(:each) do
-    @well_formatted_input = ParsedInput.new("from penn station, new york to grand central, new york")
-    @well_formatted_shorthand_input = ParsedInput.new("penn station, new york to grand central, new york")
+    @input = ParsedInput.new("bike from penn station, new york to grand central, new york")
   end
+
   describe "#origin" do
     it "should get the correct input from a text" do
-      expect(@well_formatted_input.origin).to eq "penn station, new york"
-    end
-
-    it "should get the correct input from a text that omits the 'from' keyword" do
-      expect(@well_formatted_shorthand_input.origin).to eq "penn station, new york"
+      expect(@input.origin).to eq "penn station new york"
     end
   end
 
   describe "#destination" do
     it "should get the correct input from a text" do
-      expect(@well_formatted_input.destination).to eq "grand central, new york"
-    end
-
-    it "should get the correct input from a text that omits the 'from' keyword" do
-      expect(@well_formatted_shorthand_input.destination).to eq "grand central, new york"
+      expect(@input.destination).to eq "grand central new york"
     end
   end
-  
-  describe "#mode"
+
+  describe "#mode" do
+    it "should return the correct transportation type" do
+      expect(@input.mode).to eq "bicycling"
+    end
+  end
 end
